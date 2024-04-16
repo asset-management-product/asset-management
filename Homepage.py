fca2c1774dc1d2886bad96013e37f3926e10bb3a
asset-management-product <167235228+asset-management-product@users.noreply.github.com>
2024-04-17 00:15:31 +0700

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
+
+st.title("Main Page")
+
+
+
+st.write("Requirement is updated in this file: https://1drv.ms/p/s!Agfa0F4-51Tw6DXYCqd6kQA3hpEA?e=cMOoaA")
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# asset-management
+# master
